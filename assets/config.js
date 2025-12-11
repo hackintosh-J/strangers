38ab6cf41651a3f3164538db1c48436a0ec79cf8
hackintosh-J <chenxinghao2318@163.com>
2025-12-12 06:31:18 +0800

    Update API URL for production

--- assets/config.js
+++ assets/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // 生产环境 (绑定域名后):
   // 我们将配置 Worker 路由到 https://hackins.club/api/*
   // 所以这里填写根域名，或者即使留空(使用相对路径)也可以
-  apiUrl: 'https://hackins.club',
+  apiUrl: 'https://strangers-backend.chenxinghao2318.workers.dev',
 
   // 如果您在本地开发，或者域名还未生效，
   // 可以临时改为您的 workers.dev 地址 (但在国内需翻墙)
